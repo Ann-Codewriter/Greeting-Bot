e4e719b9a0d9f06a1743b247b33e12a71e274426
Ann-Codewriter <qantoinnette23@gmail.com>
2025-10-20 20:26:09 +0000

    Add second sentence.

--- js/sentenceMaker.js
+++ js/sentenceMaker.js
@@ -21,3 +21,10 @@ verb = "hunting";
 place = "in the forest";
 adjective2 = "big";
 noun2 = "grasscutters";
+
+let secondStory = "Once upon a time, " + "there was a " + adjective 
++ " " + noun + " who loved to eat " + noun2 + ". " + "The " + noun 
++ " lived in a " + place + " and had " + adjective2 + 
+" nostrils that blew fire when it was " + verb + ".";
+
+console.log ("Second story:" + secondStory);
