dea16f8637eb8798d2d83523cba6a40c03f11130
Ann-Codewriter <qantoinnette23@gmail.com>
2025-10-20 13:28:08 +0000

    Initialize two variables.

--- js/greetingBot.js
+++ js/greetingBot.js
@@ -1,11 +1,13 @@
 /*Use variables, data types and simple arithmetic operators to build a greeting bot */
 
 //Step 1 - Add a message
-
 console.log("Hi, there!");
 
 //Step 2 - Add another statement
-
 console.log("I am excited to talk to you.");
 
+//Step 3 - Declare and initialize a variable
+let bot;
+bot = "teacherBot";
+let botLocation = "the universe";
 
