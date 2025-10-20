3a570c685b3fd4233519a9a4f30bf339bf698763
Ann-Codewriter <qantoinnette23@gmail.com>
2025-10-20 20:17:46 +0000

    Add first sentence.

---
+++ js/sentenceMaker.js
@@ -0,0 +1,14 @@
+//Create variables
+let adjective = "black";
+let noun = "dragon";
+let verb = "eating";
+let place = "pit";
+let adjective2 = "long";
+let noun2 = "birds";
+
+let firstStory = "Once upon a time, " + "there was a " 
++ adjective + " " + noun + " who loved to eat " + noun2 + ". " 
++ "The " + noun + " lived in a " + place + " and had " + 
+adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
