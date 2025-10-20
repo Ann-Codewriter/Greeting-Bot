bf0dc205d546e9e90ebe2fa2a03c6bbb1ace5d34
Ann-Codewriter <qantoinnette23@gmail.com>
2025-10-20 19:34:26 +0000

    Add name, location and favorite language.

---
+++ js/triviaBot.js
@@ -0,0 +1,14 @@
+//Building a JavaScript Trivia Bot
+
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Trivia";
+let botLocation = "Amsterdam";
+let favoriteLanguage = "JavaScript";
+console.log("My name is " + botName + " " + "and I live in " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " makes webpages alive.";
+console.log(codingFact);
+
