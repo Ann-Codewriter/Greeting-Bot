b30ecd272d17c157c98d8d7bd2667793bce13223
Ann-Codewriter <qantoinnette23@gmail.com>
2025-10-22 16:09:11 +0000

    Add an introductory statement.

---
+++ js/teacherBot.js
@@ -0,0 +1,7 @@
+//Create a teacher bot with variables, template literals and strings interpolation
+
+console.log("Hi there!");
+
+let botName = "teacherBot";
+let greeting = `My name is ${botName}.`;
+console.log(greeting);
