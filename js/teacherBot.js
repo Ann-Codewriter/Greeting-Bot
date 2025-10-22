a0a158bd7219e5d105d8e6f718093345c3c15fcb
Ann-Codewriter <qantoinnette23@gmail.com>
2025-10-22 16:11:53 +0000

    Add a new sentence.

--- js/teacherBot.js
+++ js/teacherBot.js
@@ -5,3 +5,11 @@ console.log("Hi there!");
 let botName = "teacherBot";
 let greeting = `My name is ${botName}.`;
 console.log(greeting);
+
+let subject = "JavaScript";
+let topic = "strings";
+
+let sentence = `Today, you will learn about ${topic} in ${subject}.`;
+console.log(sentence);
+
+//String length property
