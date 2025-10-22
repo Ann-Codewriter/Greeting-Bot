65f5c4fe4b62d7bf19cb7c926187b41be8ac5031
Ann-Codewriter <qantoinnette23@gmail.com>
2025-10-22 16:20:14 +0000

    Find the length of the topic string.

--- js/teacherBot.js
+++ js/teacherBot.js
@@ -15,3 +15,10 @@ console.log(sentence);
 //String length property
 let strlengthIntro = `Here is an example of using the length property on the word ${subject}.`;
 console.log(strlengthIntro);
+
+console.log(subject.length);
+
+//Find length of the topic string
+
+console.log(`Here is an example of using the length property on the word ${topic}.`);
+console.log(topic.length);
