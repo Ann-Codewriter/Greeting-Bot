ccbfe9ccc7d2e13c65aa77325b93107618ca7aab
Ann-Codewriter <qantoinnette23@gmail.com>
2025-11-07 13:25:30 +0000

    Find the total cost of cups sold.

--- js/functions.js
+++ js/functions.js
@@ -1,25 +1,35 @@
 //Boolean check function
 
-function dataType(x){
-  if (x === true){
-    return true
-  } 
-  else if (x === false){
-    return true
-  }
-  else {
-    return false
-  }
+// function dataType(x){
+//   if (x === true){
+//     return true
+//   } 
+//   else if (x === false){
+//     return true
+//   }
+//   else {
+//     return false
+//   }
+// }
+
+// console.log(dataType(true));
+// console.log(dataType(false));
+// console.log(dataType([1, 2, 3]));
+// console.log(dataType([].slice));
+// console.log(dataType({ "a": 1 }));
+// console.log(dataType(1));
+// console.log(dataType(NaN));
+// console.log(dataType("a"));
+// console.log(dataType("true"));
+// console.log(dataType("false"));
+
+
+//The Coffee Shop
+
+function totalCost(unitPrice, numberOfCupsSold){
+  return unitPrice * numberOfCupsSold
 }
 
-console.log(dataType(true));
-console.log(dataType(false));
-console.log(dataType([1, 2, 3]));
-console.log(dataType([].slice));
-console.log(dataType({ "a": 1 }));
-console.log(dataType(1));
-console.log(dataType(NaN));
-console.log(dataType("a"));
-console.log(dataType("true"));
-console.log(dataType("false"));
+
+console.log(totalCost(11, 180));
 
