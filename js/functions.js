44308fbb75fa9dbd1aa94efa30e2c59823eaa573
Ann-Codewriter <qantoinnette23@gmail.com>
2025-11-24 21:04:56 +0000

    Remove comments.

--- js/functions.js
+++ js/functions.js
@@ -1,65 +1,65 @@
-//Boolean check function
+Boolean check function
 
-// function dataType(x){
-//   if (x === true){
-//     return true
-//   } 
-//   else if (x === false){
-//     return true
-//   }
-//   else {
-//     return false
-//   }
-// }
+function dataType(x){
+  if (x === true){
+    return true
+  } 
+  else if (x === false){
+    return true
+  }
+  else {
+    return false
+  }
+}
 
-// console.log(dataType(true));
-// console.log(dataType(false));
-// console.log(dataType([1, 2, 3]));
-// console.log(dataType([].slice));
-// console.log(dataType({ "a": 1 }));
-// console.log(dataType(1));
-// console.log(dataType(NaN));
-// console.log(dataType("a"));
-// console.log(dataType("true"));
-// console.log(dataType("false"));
+console.log(dataType(true));
+console.log(dataType(false));
+console.log(dataType([1, 2, 3]));
+console.log(dataType([].slice));
+console.log(dataType({ "a": 1 }));
+console.log(dataType(1));
+console.log(dataType(NaN));
+console.log(dataType("a"));
+console.log(dataType("true"));
+console.log(dataType("false"));
 
 
 //The Coffee Shop
 
-// function totalCost(unitPrice, numberOfCupsSold) {
-//   return unitPrice * numberOfCupsSold;
-// }
+function totalCost(unitPrice, numberOfCupsSold) {
+  return unitPrice * numberOfCupsSold;
+}
 
-// function applyDiscount(unitPrice, numberOfCupsSold, discount) {
-//   const cost = totalCost(unitPrice, numberOfCupsSold);
-//   const discountApplied = cost * (discount / 100);
+function applyDiscount(unitPrice, numberOfCupsSold, discount) {
+  const cost = totalCost(unitPrice, numberOfCupsSold);
+  const discountApplied = cost * (discount / 100);
 
-//   if (numberOfCupsSold > 10) {
-//     return cost - discountApplied;
-//   } 
-//   else {
-//     return cost;
-//   }
-// }
+  if (numberOfCupsSold > 10) {
+    return cost - discountApplied;
+  } 
+  else {
+    return cost;
+  }
+}
 
-// function currencyConversion(amount, exchangeRate) {
-//   return amount * exchangeRate;
-// }
+function currencyConversion(amount, exchangeRate) {
+  return amount * exchangeRate;
+}
 
-// function finalPrice(unitPrice, numberOfCupsSold, discount, exchangeRate) {
-//   const discountedPrice = applyDiscount(unitPrice, numberOfCupsSold, discount);
-//   const convertedPrice = currencyConversion(discountedPrice, exchangeRate);
+function finalPrice(unitPrice, numberOfCupsSold, discount, exchangeRate) {
+  const discountedPrice = applyDiscount(unitPrice, numberOfCupsSold, discount);
+  const convertedPrice = currencyConversion(discountedPrice, exchangeRate);
 
-//   return convertedPrice;
-// }
+  return convertedPrice;
+}
 
-// const unitPrice = 11;
-// const numberOfCupsSold = 180;
-// const discount = 15;
-// const exchangeRate = 11.50;
+const unitPrice = 11;
+const numberOfCupsSold = 180;
+const discount = 15;
+const exchangeRate = 11.50;
 
-// console.log("Total Cost:", totalCost(unitPrice, numberOfCupsSold));
-// console.log("Final Price:", finalPrice(unitPrice, numberOfCupsSold, discount, exchangeRate));
+console.log("Total Cost:", totalCost(unitPrice, numberOfCupsSold));
+console.log("Final Price:", finalPrice(unitPrice, numberOfCupsSold, discount, exchangeRate));
 
 
 //Warehouse Inventory
@@ -98,6 +98,8 @@ inventory = removeStock(inventory, 25);
 console.log("Final Inventory Count:", inventory);
 console.log(checkRestock(inventory));
 
+
+//Student Grades Evaluator
 
 
 
