786ff8304752e028f0872aea1a3e1b24cd4fa476
Ann-Codewriter <qantoinnette23@gmail.com>
2025-11-27 15:35:22 +0000

    Find average score for each student.

--- js/studentGradesEvaluator.js
+++ js/studentGradesEvaluator.js
@@ -1,71 +1,20 @@
-//To find the average score of 3 subject marks,
+//Declare 3 variables that contains the data of each student.
 
-let student1 = {
-    name: "Ama",
-    scores: [56, 62, 77]
-};
+let students = [
+    {name: "Ama", scores: [56, 62, 77]},
+    {name: "Kofi", scores: [68, 43, 73]},
+    {name: "Yaw", scores: [68, 97, 32]}
+];
 
-let student2 = {
-    name: "Kofi",
-    scores: [68, 43, 73]
-};
-
-let student3 = {
-    name: "Yaw",
-    scores: [56, 62, 77]
-};
-
-
-
-function averageScore(mark1, mark2, mark3){
-  return (mark1 + mark2 + mark3) / 3;
+//Calculate the averageScore of students subject marks by first adding the marks and dividing by 3.
+function averageScore(student){
+  let totalScore = 0;
+  for (let i = 0; i < student.scores.length; i++){
+    totalScore = totalScore + student.scores[i];
+  }
+  return totalScore / student.scores.length;
 }
 
-//To assign a grade based on the average score, a new function named assignGrade will be called.
-function assignGrade(average){
-  //Use if, else if and else statements
-  if(average >= 80) {
-    return "A";
-  }
-  else if (average >= 70){
-    return "B";
-  }
-  else if (average >= 60){
-    return "C";
-  }
-  else if (average >= 50){
-    return "D";
-  }
-  else {
-    return "F";
-  }
-}
-
-//To check whether the student has passed or failed, call another function named checkStatus.
-function checkStatus(average){
-  if (average >= 60){
-    return "Passed";
-  }
-  else {
-    return "Failed";
-  }
-}
-
-console.log(checkStatus(averageScore))
-
-//To test three students with the functions, a new function named studentTest
-function studentTest(name, mark1, mark2, mark3){
-
-  const avg = averageScore(mark1, mark2, mark3);
-  const grade = assignGrade(avg);
-  const passed = checkStatus(grade);
-
-  console.log(`Student: ${name}`);
-  console.log(`Average Score: ${avg.toFixed(2)}`);
-  console.log(`Grade: ${grade}`);
-
-}
-
-console.log(studentTest("Ama", 67, 81, 72));
-// console.log(studentTest("Kofi", 69, 64, 80));
-// console.log(studentTest("Yeboah", 96, 90, 76));
+students.forEach(student => {
+    console.log(student.name + "'s average is:", averageScore(student).toFixed(2));
+});
